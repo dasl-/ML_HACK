855dc7c015a5ec7f255c6c73b105233ac881fb26
Ryan Bateman <rbateman@etsy.com>
2014-02-22 18:52:26 -0500

    Bus test

--- bus_test/bus_test.ino
+++ bus_test/bus_test.ino
@@ -10,18 +10,16 @@ Midilooper_Bus bus = Midilooper_Bus();
  * Initialize the Arduino
  */
 void setup(){
-    Serial.begin(9600);
+    
 }
 
 /*
  * The main execution loop
  */
 void loop() {
-    Serial.println("loop");
     byte args[2];
     args[0] = (byte) 60;
     args[1] = (byte) 120;
-    bus.send(Midilooper_Instruction::NOTE_ON, args);
-    bus.get();
-    delay(2000);
+    bus.run(Midilooper_Instruction::NOTE_ON, args);
+    delay(1000);
 }
