1ee3deb590dfec0865c8a1ed35b74e690f5ddf07
davidleibovic <david.leibovic@gmail.com>
2014-02-22 16:45:14 -0500

    refactor the code to call the different light modes. refactor delay times into a variable

--- led_test/led_test.ino
+++ led_test/led_test.ino
@@ -12,6 +12,9 @@ int blue;
 float ramp_time = 50;
 int base_pixel = 8;
 
+/** @type {Number} control the delay() times */
+int tempo = 500;
+
 #include <Adafruit_NeoPixel.h>
 Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(21, 6, NEO_GRB + NEO_KHZ800);
 
@@ -21,7 +24,6 @@ Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(21, 6, NEO_GRB + NEO_KHZ800);
 void setup(){
     Serial.begin(9600);
     pixels = strip_1.numPixels();
-    
     strip_1.begin();
     strip_1.show();
 }
@@ -30,159 +32,152 @@ void setup(){
  * The main execution loop
  */
 void loop() {
-//    //EQ
-//    red = random(255);
-//    green = random(255);
-//    blue = random(255);
-//    
-//    eq_setStripInstruction(60, 127, red, green, blue);
-//    eq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        eq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    eq_setStripInstruction(60, 100, red, green, blue);
-//    eq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        eq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    eq_setStripInstruction(60, 64, red, green, blue);
-//    eq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        eq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    eq_setStripInstruction(60, 1, red, green, blue);
-//    eq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        eq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    
-//    
-//    
-//    
-    //Flash Color
+    // _eq();
+    _flashColor();
+    // _splitEq();
+    // _doubleEq();
+}
+
+void _eq() {
+    red = random(255);
+    green = random(255);
+    blue = random(255);
+
+    eq_setStripInstruction(60, 127, red, green, blue);
+    eq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       eq_runStripInstruction();
+    }
+    delay(tempo);
+
+    eq_setStripInstruction(60, 100, red, green, blue);
+    eq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       eq_runStripInstruction();
+    }
+    delay(tempo);
+
+    eq_setStripInstruction(60, 64, red, green, blue);
+    eq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       eq_runStripInstruction();
+    }
+    delay(tempo);
+
+    eq_setStripInstruction(60, 1, red, green, blue);
+    eq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       eq_runStripInstruction();
+    }
+    delay(tempo);
+}
+
+void _flashColor() {
     red = random(8);
     green = random(8);
     blue = random(8);
-    
+
     while (red + green + blue <= 2) {
         red = random(8);
         green = random(8);
         blue = random(8);
     }
-    
+
     flashColor_setStripInstruction(60, 127, red, green, blue);
     flashColor_runStripInstruction();
     while (instruction_step <= instruction_length + 10) {
         flashColor_runStripInstruction();
     }
-    delay(500);
-    
+    delay(tempo);
+
     flashColor_setStripInstruction(60, 100, red, green, blue);
     flashColor_runStripInstruction();
     while (instruction_step <= instruction_length + 10) {
         flashColor_runStripInstruction();
     }
-    delay(500);
-    
+    delay(tempo);
+
     flashColor_setStripInstruction(60, 64, red, green, blue);
     flashColor_runStripInstruction();
     while (instruction_step <= instruction_length + 10) {
         flashColor_runStripInstruction();
     }
-    delay(500);
-    
+    delay(tempo);
+
     flashColor_setStripInstruction(60, 30, red, green, blue);
     flashColor_runStripInstruction();
     while (instruction_step <= instruction_length + 10) {
         flashColor_runStripInstruction();
     }
-    delay(500);
+    delay(tempo);
+}
 
+void _splitEq() {
+    red = random(255);
+    green = random(255);
+    blue = random(255);
 
+    splitEq_setStripInstruction(60, 127, red, green, blue);
+    splitEq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       splitEq_runStripInstruction();
+    }
+    delay(tempo);
 
+    splitEq_setStripInstruction(60, 100, red, green, blue);
+    splitEq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       splitEq_runStripInstruction();
+    }
+    delay(tempo);
 
+    splitEq_setStripInstruction(60, 82, red, green, blue);
+    splitEq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       splitEq_runStripInstruction();
+    }
+    delay(tempo);
 
+    splitEq_setStripInstruction(60, 60, red, green, blue);
+    splitEq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       splitEq_runStripInstruction();
+    }
+    delay(tempo);
+}
 
-//
-//    //SPLIT EQ
-//    red = random(255);
-//    green = random(255);
-//    blue = random(255);
-//    
-//    splitEq_setStripInstruction(60, 127, red, green, blue);
-//    splitEq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        splitEq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    splitEq_setStripInstruction(60, 100, red, green, blue);
-//    splitEq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        splitEq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    splitEq_setStripInstruction(60, 82, red, green, blue);
-//    splitEq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        splitEq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    splitEq_setStripInstruction(60, 60, red, green, blue);
-//    splitEq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        splitEq_runStripInstruction();
-//    }
-//    delay(500);
+void _doubleEq() {
+    red = random(255);
+    green = random(255);
+    blue = random(255);
 
+    doubleEq_setStripInstruction(60, 127, red, green, blue);
+    doubleEq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       doubleEq_runStripInstruction();
+    }
+    delay(tempo);
 
+    doubleEq_setStripInstruction(60, 100, red, green, blue);
+    doubleEq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       splitEq_runStripInstruction();
+    }
+    delay(tempo);
 
+    doubleEq_setStripInstruction(60, 82, red, green, blue);
+    doubleEq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       doubleEq_runStripInstruction();
+    }
+    delay(tempo);
 
-
-
-
-    //DOUBLE EQ
-//    red = random(255);
-//    green = random(255);
-//    blue = random(255);
-//    
-//    doubleEq_setStripInstruction(60, 127, red, green, blue);
-//    doubleEq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        doubleEq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    doubleEq_setStripInstruction(60, 100, red, green, blue);
-//    doubleEq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        splitEq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    doubleEq_setStripInstruction(60, 82, red, green, blue);
-//    doubleEq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        doubleEq_runStripInstruction();
-//    }
-//    delay(500);
-//    
-//    doubleEq_setStripInstruction(60, 60, red, green, blue);
-//    doubleEq_runStripInstruction();
-//    while (instruction_step <= instruction_length + 10) {
-//        doubleEq_runStripInstruction();
-//    }
-//    delay(500);
+    doubleEq_setStripInstruction(60, 60, red, green, blue);
+    doubleEq_runStripInstruction();
+    while (instruction_step <= instruction_length + 10) {
+       doubleEq_runStripInstruction();
+    }
+    delay(tempo);
 }
 
 float a = 3.351;
--- led_test/splitEq.ino
+++ led_test/splitEq.ino
@@ -2,15 +2,15 @@ void splitEq_setStripInstruction(byte pitch, byte velocity, int red, int green, 
     instruction_start = millis();
     instruction_length = 200;
     instruction_start_val = (expScale(velocity) * pixels) / 255;
-    
+
     instruction_args[0] = pitch;
     instruction_args[1] = velocity;
     instruction_args[2] = red;
     instruction_args[3] = green;
     instruction_args[4] = blue;
-    
+
     uint32_t color = strip_1.Color(red, green, blue);
-    
+
     for(uint16_t i=0; i < pixels; i++) {
         strip_1.setPixelColor(i, strip_1.Color(0, 0, 0));
     }
@@ -20,7 +20,7 @@ void splitEq_setStripInstruction(byte pitch, byte velocity, int red, int green, 
 void splitEq_runStripInstruction() {
     unsigned long clock = millis();
     instruction_step = clock - instruction_start;
-    
+
     if (instruction_step <= instruction_length) {
         byte pitch = instruction_args[0];
         byte velocity = instruction_args[1];
@@ -28,12 +28,12 @@ void splitEq_runStripInstruction() {
         byte green = instruction_args[3];
         byte blue = instruction_args[4];
         uint32_t color = strip_1.Color(red, green, blue);
-        
+
         if (instruction_step <= ramp_time) {
             //go up in the first 100 ms
             float fade_by = instruction_start_val / (float) ramp_time;
             int set_val = (instruction_step * fade_by);
-        
+
             for(uint16_t i=0; i < pixels; i++) {
                 if (i <= (set_val / 2) || i >= (pixels - (set_val / 2))) {
                     strip_1.setPixelColor((i + base_pixel) % pixels, color);
@@ -45,7 +45,7 @@ void splitEq_runStripInstruction() {
             //go down after the first 100 ms
             float fade_by = instruction_start_val / (float) (instruction_length - ramp_time);
             int set_val = instruction_start_val - ((instruction_step - ramp_time) * fade_by);
-            
+
             for(uint16_t i=0; i < pixels; i++) {
                 if (i <= (set_val / 2) || i >= (pixels - (set_val / 2))) {
                     strip_1.setPixelColor((i + base_pixel) % pixels, color);
